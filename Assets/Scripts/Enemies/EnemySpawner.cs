4463a06113005a80d8f227ea2402207f38630575
xaita <xiao.shan.costajussa@estudiantat.upc.edu>
2024-12-16 21:21:09 +0100

    Starting Enemies AI with NavMesh

---
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] int enemiesPerWave = 5;
+    [SerializeField] float spanInterval = 1f;
+    private bool isActive = false;
+    private int currentWave = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        while (isActive)
+        {
+            currentWave++;
+            for (int i = 0; i< enemiesPerWave; i++)
+            {
+                SpawnEnemy();
+                yield return new WaitForSeconds(spanInterval);
+            }
+            isActive = false;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+    }
+}
